eda42e79cb2edc0903438d4805ac57ed8f92590a
Caio Cesar <caio.sfg@gmail.com>
2021-06-29 23:50:44 -0300

    Modelando os resultados de jogos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
+  <div class="events">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    EventCard,
   },
+  data() {
+    return {
+      events: [
+        {
+        id: 171,
+        category: 'lotofacil',
+        title: 'Joso de Junho',
+        description: 'Arranjo combinado',
+        location: 'Campo Grande',
+        date: '29 Junho, 2021',
+        time: '23:00',
+        gameBet: true,
+        organizer: 'Angelo'
+      },
+      {
+        id: 172,
+        category: 'megasena',
+        title: 'Joso de Maio',
+        description: 'Arranjo combinado 2',
+        location: 'Campo Grande',
+        date: '30 Junho, 2021',
+        time: '23:00',
+        gameBet: true,
+        organizer: 'Caio'
+      }
+    ]
+    }
+  }
 };
 </script>
+
+<style scoped>
+ .events{
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+ }
+</style>
